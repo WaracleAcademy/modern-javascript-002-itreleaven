d78ad8359bc413dce9897dfc781456c5a5476d6a
Ian <i.treleaven@waracle.com>
2019-10-01 18:07:36 +0100

    moved js to own file

---
+++ index.js
@@ -0,0 +1,6 @@
+const things  = ['Voyager','Enterprise','Defiant'];
+
+const mainView = document.querySelector('#primaryView');
+
+mainView.innerHTML = '<h2>Text</h2>';
+//console.log('this is javascript Ian'); 
