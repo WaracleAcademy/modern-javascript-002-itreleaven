1c1145c3c35d4e9810592f26c742202e3fcb3366
Ian <i.treleaven@waracle.com>
2019-10-22 19:42:01 +0100

    lesson 6 - add event listeners

--- index.js
+++ index.js
@@ -6,7 +6,26 @@ import * as data from './data.json';
 
 function getThingTemplate(thing){
 	
-	return `<li>${thing.name} ${thing.registry}</li>`;;
+	return `<li class="list-item" data-id="${thing.id}">${thing.name} ${thing.registry}</li>`; //back ticks important to use strings
+
+};
+
+function getThingDetails(thing){
+	
+	return `
+	<nav>
+		<button class="back-button">
+			Back
+		</button>
+		</nav>
+	<article>
+      <h1>Name: ${thing.name}</h1>
+      <p>Class: ${thing.class}</p>
+      <p>Registry: ${thing.registry}</p>
+    </article>
+
+
+	`; //back ticks important to use strings
 
 };
 
@@ -26,27 +45,82 @@ else{
 	return 0;
 }
 }
+
+function findById(things,id){
+	return things.find(function(thing){// returns item in the list that matches
+	//console.log(result);
+	return thing.id == id; //returns true or false
+	});
+}
 	
 
-console.log(data.results);
+//console.log(data.results);
 //console.log(data.results[0]);
 
-const things =data.results.sort(sortMyStuff);
-let content = '<ul>';
+function renderList(things){
+	let content = '<ul>';
 
-for(let i=0; i < things.length; i++){
+	for(let i=0; i < things.length; i++){
 
-console.log(things[i]);
-content += getThingTemplate(things[i]);
+	//console.log(things[i]);
+	content += getThingTemplate(things[i]);
 
 
+
+	};
+	content += '</ul>';
+
+	render(content);
+	//getThingTemplate({});	
+
+
+
+
+const allLists = document.querySelectorAll('.list-item');
+
+allLists.forEach(function(item){
+
+	
+	item.addEventListener('click',function(event){
+		//console.log(item);
+		const ship = findById(things,item.dataset.id);
+		console.log(ship);
+		//content += getThingDetails(ship); //testing
+		//render(content);	//testing
+		const template = getThingDetails(ship);
+		//console.log(template);
+		render(template);
+		const backButton = document.querySelector('.back-button');
+		backButton.addEventListener('click',function(event){
+
+			renderList(things);
+		});
+
+		//getThingDetails({});
+		//console.log(item.dataset.id);
+	});
+
+});
 
 };
-content += '</ul>';
 
-render(content);
-getThingTemplate({});
-console.log('this is a feature change');
+
+const things =data.results.sort(sortMyStuff);
+
+renderList(things);
+//console.log(allLists);
+
+
+
+// const ship = things.find(function(item){
+// return item.id == "1";
+
+
+// });
+// console.log(ship);
+
+
+//console.log('this is a feature change');
 
 //const things  = ['Voyager','Enterprise','Defiant'];
 
